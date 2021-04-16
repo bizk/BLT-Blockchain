66b21c942ad5989ce7dbccd7502831a78f43b9f2
Bizk <santiago.yanzon1999@gmail.com>
2021-04-16 16:13:50 -0300

    Added typescreipt project

---
+++ Block.ts
@@ -0,0 +1,10 @@
+ class Block {
+    private index: int;
+    private timestamp: long;
+    private hash: string;
+    private previousHash: string;
+    private data: string;
+    private nonce: int;
+
+
+}
