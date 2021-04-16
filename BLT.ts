b174fe3e330cc84c01915eed71b9370e01672011
Bizk <santiago.yanzon1999@gmail.com>
2021-04-16 18:25:09 -0300

    Added blocks and hash operations

--- BLT.ts
+++ BLT.ts
@@ -1,5 +1,20 @@
 const world = 'world';
 
+import { sha256 } from 'js-sha256';
+
+import { Block } from "./Block";
+
 export function hello(word: string = world): string {
   return `Hello ${world}! `;
+}
+
+function digest(block:Block): string {
+    if (block !== null) {
+       return sha256(block.toString())
+    }
+    return ""
+}
+
+function mineBlock(params:type) {
+  
 }
--- Block.ts
+++ Block.ts
@@ -1,4 +1,7 @@
- class Block {
+  import { sha256 } from 'js-sha256';
+
+ export class Block {
+    private objectString: string;
     private index: int;
     private timestamp: long;
     private hash: string;
@@ -6,5 +9,15 @@
     private data: string;
     private nonce: int;
 
+    constructor() {
+        let rand = Math.random().toString(16).substr(2, 8); 
+        this.objectString = rand //We simulate the string value of the whole object bytes to save time
+        this.hash = sha256(rand)
 
+    }
+
+    //This should convert the whole object to a string
+    toString(): string {
+        return this.objectString //We return this simulation of string
+    }
 }
