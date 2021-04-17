e43af5e701cb06dbdb5d25dc6121ad2ad3c83f34
Bizk <santiago.yanzon1999@gmail.com>
2021-04-17 18:17:39 -0300

    Added blockchain class

--- Blockchain.ts
+++ Blockchain.ts
@@ -2,9 +2,25 @@ import { Block } from "./Block";
 
 export class Blockchain {
     private difficulty: int;
-    private blocks: Block[];
+    private blocks: Array<Block>;
     
     constructor(parameters) {
         
+    }
+
+    latestBlock(): Block {
+        return this.blocks[this.blocks.length - 1];
+    }
+
+    newBlock(data: string): Block {
+        latestBlock: Block = this.latestBlock();
+        return new Block(latestBlock.getIndex() + 1, Date.now(), latestBlock.getHash(), data);
+    }   
+
+    addBlock(block: Block) {
+        if (block != null) {
+            block.mineBlock(this.difficulty);
+            this.blocks.push(block)
+        }
     }
 }
