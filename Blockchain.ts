0c66d4951f83e08b1037bde79952190985a32e89
Bizk <santiago.yanzon1999@gmail.com>
2021-04-17 19:32:33 -0300

    Added blockchain nchecks for nodes and blocks

--- Blockchain.ts
+++ Blockchain.ts
@@ -1,4 +1,5 @@
 import { Block } from "./Block";
+import { calculateHash } from "./utils";
 
 export class Blockchain {
     private difficulty: number;
@@ -22,5 +23,38 @@ export class Blockchain {
             block.mineBlock(this.difficulty);
             this.blocks.push(block)
         }
+    }
+
+    isFirstBlockValid(): boolean {
+        let firstBlock: Block = this.blocks[0];
+
+        if (firstBlock.getIndex() !== 0) return false;
+        if (firstBlock.getPreviousHash() !== null) return false;
+        if (firstBlock.getHash() === null || calculateHash(firstBlock) !== firstBlock.getHash()) return false;
+
+        return true;
+    }
+
+    isValidNewBlock(newBlock: Block, previousBlock: Block): boolean {
+        if (newBlock !== null && previousBlock !== null) {
+            if (newBlock.getIndex() + 1 !== newBlock.getIndex()) return false;
+            if (newBlock.getPreviousHash() === null || newBlock.getPreviousHash() !== previousBlock.getHash()) return false
+            if (newBlock.getHash() === null || newBlock.getHash() !== calculateHash(newBlock)) return false
+
+            return true
+        }
+        return false 
+    }
+
+    isBlockChainValid(): boolean {
+        if (!this.isFirstBlockValid()) return false;
+
+        for (let index = 0; index < this.blocks.length; index++) {
+            let currentBlock: Block = this.blocks[index]
+            let previousBlock: Block = this.blocks[index - 1] 
+            
+            if (!this.isValidNewBlock(currentBlock, previousBlock)) return false;
+        }
+        return true
     }
 }
--- utils.ts
+++ utils.ts
@@ -1,5 +1,16 @@
+import { sha256 } from "js-sha256";
+import { Block } from "./Block";
 
+    function generateRandomString() {
+        return Math.random().toString(16).substr(2, 8)
+    }
+    export {generateRandomString};
+    
+     function calculateHash(block: Block): String {
+        let blockString: String = block.str();
+        let blockDigest: String = sha256(blockString.toString())
+    
+        return blockDigest
+    }
 
-function generateRandomString() {
-    return Math.random().toString(16).substr(2, 8)
-}
+    export {calculateHash};
